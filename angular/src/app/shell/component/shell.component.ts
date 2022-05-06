f973d85754b4c1e83408159471b2fa701544de51
Santiago Gerhart <gerhartsantiago@gmail.com>
2022-05-05 22:59:15 -0300

    styles: remove unused code

--- angular/src/app/shell/component/shell.component.ts
+++ angular/src/app/shell/component/shell.component.ts
@@ -1,18 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-shell',
   templateUrl: './shell.component.html',
   styleUrls: ['./shell.component.css'],
 })
-export class ShellComponent implements OnInit {
+export class ShellComponent {
   public isSidenavOpened: boolean = false;
 
   onSidenavButton() {
     this.isSidenavOpened = !this.isSidenavOpened;
   }
-
-  constructor() {}
-
-  ngOnInit(): void {}
 }
