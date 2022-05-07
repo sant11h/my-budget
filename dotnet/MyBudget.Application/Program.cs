72e4f92007330c62e0c8a905f8ef228db468e86e
Santiago Gerhart <gerhartsantiago@gmail.com>
2022-05-07 06:07:41 -0300

    feat: add dotnet to docker compose

--- dotnet/MyBudget.Application/Program.cs
+++ dotnet/MyBudget.Application/Program.cs
@@ -2,8 +2,6 @@ using MyBudget.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.UseUrls("http://0.0.0.0:5000", "https://0.0.0.0:5001");
-
 builder.Services.AddControllersWithViews();
 
 builder.Services.Configure<MongoOptions>(builder.Configuration.GetSection(MongoOptions.Mongo));
